2c1750cb202a7e1f8a77e7fa516a8caf75d7946c
Luis Álvarez <luigi>
2025-09-20 17:48:53 -0400

    cambios para ver detalles de post

--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,15 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
+
 
 # Create your views here.
 class PostListView(ListView):
     model = Post
-    template_name = 'post_list.html'
-    context_object_name = 'post_list'
-    
+    template_name = "post_list.html"
+    context_object_name = "post_list"
 
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    context_object_name = "post"
